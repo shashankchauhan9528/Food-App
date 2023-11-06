56a13a6e4664ceac25964c85006515216066a980
Shashank Chauhan <shashankchauhan1332@gmail.com>
2023-11-06 16:42:09 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.use("/images", express.static(path.join(__dirname, "../images")));
+
+app.get("/", (req, res) => {
+  console.log(path.join(__dirname, "../public"));
+  const foodData = [
+    {
+      name: "Boiled Egg",
+      price: 10,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/egg.png",
+      type: "breakfast",
+    },
+    {
+      name: "RAMEN",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/ramen.png",
+      type: "lunch",
+    },
+    {
+      name: "GRILLED CHICKEN",
+      price: 45,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/chicken.png",
+      type: "dinner",
+    },
+    {
+      name: "CAKE",
+      price: 18,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/cake.png",
+      type: "breakfast",
+    },
+    {
+      name: "BURGER",
+      price: 23,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/burger.png",
+      type: "lunch",
+    },
+    {
+      name: "PANCAKE",
+      price: 25,
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.",
+      image: "/images/pancake.png",
+      type: "dinner",
+    },
+  ];
+
+  res.json(foodData);
+});
+
+app.get('/', function (req, res) {
+  res.send("server started");
+});
+
+app.listen(9000);
+
+
+
+
